631620dd9eceac36a1b5b8b3d408d68e0e4af50d
Adarsh Mishra <97886091+Adarsh-Mishra1@users.noreply.github.com>
2023-04-03 17:24:02 +0530

    Update 2.DMA_array_average.c

--- C/Assignment-22/2.DMA_array_average.c
+++ C/Assignment-22/2.DMA_array_average.c
@@ -1,10 +1,13 @@
+//Program to dynamically allocate the array after taking size as input and then find the average of all the entered elements of array
 #include<stdio.h>
 #include <stdlib.h>
 int main(){
     int n,i,avg;
     int *ptr;
+    
     printf("Enter the size of array ");
     scanf("%d",&n);
+    //Here n blocks of memory allocated to ptr
     ptr=(int*)malloc(n*sizeof(int));
     
     //Enter the element
